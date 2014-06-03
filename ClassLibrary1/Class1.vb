3245b03bccb60c773cb214ef0fdceefa90bdf8d1
deepakleo2003 <deepak123!>
2014-06-03 22:17:08 +1000

    commit

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,14 @@
+﻿Public Class VBExample
+    Private teamStr As String
+    Public Sub New()
+        TeamString = "Visual Basic Code"
+    End Sub
+    Public Property TeamString() As String
+        Get
+            Return teamStr
+        End Get
+        Set(ByVal Value As String)
+            teamStr = Value
+        End Set
+    End Property
+End Class
---
+++ MvcApplication1/App_Code/CSCode/CSExample.cs
@@ -0,0 +1,19 @@
+﻿public class CSExample
+{
+    private string teamString;
+    public CSExample()
+    {
+        TeamString = "C# Code";
+    }
+    public string TeamString
+    {
+        get
+        {
+            return teamString;
+        }
+        set
+        {
+            teamString = value;
+        }
+    }
+}
